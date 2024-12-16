d9f6e80ce1a588bd71e7847083473f1a49c96490
Elisângela <elisangelaaragao644@gmail.com>
2024-12-16 20:06:05 -0300

    Update Livro.java

--- src/Livro.java
+++ src/Livro.java
@@ -1,16 +1,16 @@
 import java.io.Serializable;
    
-   //ESSA CLASSE REPRESENTA UM LIVRO GENÉRICO
+  
 public class Livro implements Serializable {
     private static final long serialVersionUID = 1L;
-    //AQUI EU JÁ DEFINI OS ATRIBUTOS DO LIVRO
+
     String titulo;
     String autor;
     String isbn;
     String genero;
     String status;
 
-    //AQUI EU USEI UM CONSTRUTOR PARA QUE OS ATRIBUTOS SEJAM INICIADOS QUANDO EU CRIAR UM NOVO OBJETO LIVRO
+   
     Livro(String titulo, String autor, String genero, String isbn)
     {
         this.titulo = titulo; 
