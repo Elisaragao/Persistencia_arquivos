3c8f3f3a5176b1a8956390b63da404e35a5b0335
Elisângela <elisangelaaragao644@gmail.com>
2024-12-16 20:08:54 -0300

    Update LivroListar.java

--- src/LivroListar.java
+++ src/LivroListar.java
@@ -1,11 +1,8 @@
 import java.util.ArrayList;
 
-//ESSA CLASSE SERVE PARA LISTAR OS LIVROS CADASTRADOS
+
 public class LivroListar {
 
-    //NESSE CASO, EU PASSEI O ARRAY QUE ARMAZENOU OS VALORES
-    //CADASTRADOS COMO PARÂMETRO
-    
     static void listarLivros(ArrayList<Livro> livros ) { 
         for (Livro livro : livros) { 
         System.out.println(
